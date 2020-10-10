4c5c588636bb510dda0e8599f59375b60ba843a9
mittalrohit0598 <mittalrohit0598@gmail.com>
2020-10-10 11:20:30 +0530

    Add #at, #contains, #find, #pop, and #prepend methods

--- linked_list.rb
+++ linked_list.rb
@@ -19,7 +19,7 @@ class LinkedList
   end
 
   def append(value)
-    if head == tail && head.value == nil
+    if head == tail && head.value.nil?
       head.value = value
     else
       tail.next_node = Node.new(value)
@@ -28,12 +28,53 @@ class LinkedList
     self.size += 1
   end
 
-  def to_s
+  def prepend(value)
+    if head == tail && head.value.nil?
+      head.value = value
+    else
+      self.head = Node.new(value, head)
+    end
+    self.size += 1
+  end
+
+  def at(index)
     node = head
-    size.times do
-      print "(#{node.value}) -> "
+    index.times do
       node = node.next_node
     end
+    node.value
+  end
+
+  def pop
+    if head == tail
+      head.value = nil
+    else
+      node = head
+      (size - 1).times { node = node.next_node }
+      tail = node
+      tail.next_node = nil
+    end
+    self.size -= 1
+  end
+
+  def contains?(value)
+    to_s.include?(value.to_s)
+  end
+
+  def find(value)
+    list = to_s
+    list.gsub('(', '').gsub(')', '').split(' -> ').find_index(value.to_s)
+  end
+
+  def to_s
+    node = head
+    list = ''
+    size.times do
+      list += "(#{node.value}) -> "
+      node = node.next_node
+    end
+    list += 'nil'
+    list
   end
 end
 
@@ -42,4 +83,16 @@ list.append(5)
 list.append(4)
 list.append(3)
 list.append(2)
+list.prepend(6)
+list.prepend(7)
+list.prepend(8)
+list.prepend(9)
 puts list
+list.pop
+list.pop
+list.pop
+puts list
+puts list.contains?(10)
+puts list.contains?(5)
+puts list.find(8)
+puts list.find(11)
